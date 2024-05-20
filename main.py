86cadc379709594242a681fc4911dbcff14285b9
Nikita <nikzakharenkov@gmail.com>
2024-05-20 14:37:57 +0300

    final first lab

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
 
 DATA_FILE = 'users.json'
+
 def load_users():
     try:
         with open(DATA_FILE, 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-
         return {'ADMIN': {'password': 'admin', 'locked': False, 'restrictions': False}}
+
 def save_users(users):
     with open(DATA_FILE, 'w') as file:
         json.dump(users, file, indent=4)
@@ -17,12 +18,13 @@ def validate_password(password, username, restrictions):
         has_latin = any(c.isalpha() and c.isascii() for c in password)
         has_cyrillic = any('а' <= c.lower() <= 'я' for c in password)
         has_digit = any(c.isdigit() for c in password)
-        return has_latin and has_cyrillic and has_digit
+        return has_latin, has_cyrillic, has_digit
     return True
+
 def admin_mode(users):
     while True:
         choice = input(
-            "1. Змінити пароль\n2. Перегляд користувачів\n3. Додати користувача\n4. Блокування користувача\n5. Змінити обмеження пароля\n6. Вийти\nВаш вибір: ")
+            "1. Змінити пароль\n2. Перегляд користувачів\n3. Додати користувача\n4. Блокування користувача\n5. Змінити обмеження пароля\n6. Довідка\n7. Вийти\nВаш вибір: ")
         if choice == '1':
             old_password = input("Введіть старий пароль: ")
             if users['ADMIN']['password'] == old_password:
@@ -64,19 +66,19 @@ def admin_mode(users):
             else:
                 print("Користувач не знайдений.")
         elif choice == '6':
+            print("Програма для управління користувачами.\nАвтор: Ім'я Автора\nВерсія: 1.0")
+        elif choice == '7':
             break
-
 
 def user_mode(user, users):
     while True:
-        choice = input("1. Змінити пароль\n2. Вийти\nВаш вибір: ")
+        choice = input("1. Змінити пароль\n2. Довідка\n3. Вийти\nВаш вибір: ")
         if choice == '1':
             old_password = input("Введіть старий пароль: ")
             if users[user]['password'] == old_password:
                 new_password = input("Введіть новий пароль: ")
                 confirm_password = input("Підтвердіть новий пароль: ")
-                if new_password == confirm_password and validate_password(new_password, user,
-                                                                          users[user]['restrictions']):
+                if new_password == confirm_password and validate_password(new_password, user, users[user]['restrictions']):
                     users[user]['password'] = new_password
                     save_users(users)
                     print("Пароль успішно змінено.")
@@ -85,8 +87,9 @@ def user_mode(user, users):
             else:
                 print("Неправильний старий пароль!")
         elif choice == '2':
+            print("Програма для управління користувачами.\nАвтор: Нікіта Захаренко ФБ-11\nВерсія: 1.0")
+        elif choice == '3':
             break
-
 
 def main():
     users = load_users()
@@ -94,7 +97,6 @@ def main():
         username = input("Ім'я користувача (або 'вийти' для завершення): ")
         if username.lower() == 'вийти':
             break
-        username = username.upper()
         if username in users:
             if users[username]['locked']:
                 print("Обліковий запис заблокований.")
@@ -116,7 +118,6 @@ def main():
                 break
         else:
             print("Користувач не знайдений. Спробуйте ще раз або зареєструйтеся як ADMIN.")
-
 
 if __name__ == '__main__':
     main()
